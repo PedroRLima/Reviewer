e2c813d5e1337010f2b811e90e3ccd17900003ba
Pedro Rodrigues Lima <perliima72@gmail.com>
2016-01-20 15:05:26 -0200

    adicionado admin e devise

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,4 +1,5 @@
 class Professor < ActiveRecord::Base
+	validates :name, presence: true 
 	acts_as_votable
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'professors#index'
   resources :professors
 end
